56e1f51fb2f0004896faecc11397a469424aa793
sbenda14 <susanbenda@u.boisestate.edu>
2020-02-18 20:43:56 -0700

    edits to contact

--- contact.php
+++ contact.php
@@ -1,9 +1,11 @@
 <?php include_once("header.php"); ?>
 		<div class="content">
 			<div class="plainText" id="contact">
-			<h1>Contact Us</h1>
-			<p> Have questions, comments or requests? Send us an email by filling out the form below!</p>
+				<h1>Contact Us</h1>
+				<p> Have questions, comments or requests? Send us an email by filling out the form below!</p>
+			</div>
+			<div class="contactForm">
+				<!-- contact form will go here. -->
 			</div>
 		</div>
-		
 <?php include_once("footer.php"); ?>
