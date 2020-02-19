8aad9b3a6564444444cd882b496cb4a6680368fd
sbenda14 <susanbenda@u.boisestate.edu>
2020-02-18 20:44:12 -0700

    edits to homepage

--- index.php
+++ index.php
@@ -1,18 +1,26 @@
 <?php include_once("header.php"); ?>
 		<div class="content">
 			<div class="plainText" id="home">
-			<h1>Welcome to The Organized Chef!</h1>
-			<ul>
-			<li>Can't remember how long to bake that potato?</li>
-			<li>Is your famous pumpkin pie recipe written on a napkin?</li>
-			<li>Have a favorite chicken recipe online...around here somewhere?</li>
-			</ul>
-			<p> 
-			This webpage is dedicated to reducing the time you
-			spend searching for your favorite recipes. With everything in one place,you can get down to the main event---cooking!
-			We provide easy to use filters to search for your favorites, or browse by ingredient if you're undecided. 
-			Get started today by joining or signing in!
-			</p>
+				<h1 class="welcome">Welcome to The Organized Chef!</h1>
+				<ul class="welcome">
+					<li>Can't remember how long to bake that potato?</li>
+					<li>Is your famous pumpkin pie recipe written on a napkin?</li>
+					<li>Have a favorite chicken recipe online...around here somewhere?</li>
+				</ul>
+				<p> 
+				This webpage is dedicated to reducing the time you
+				spend searching for your favorite recipes. With everything in one place,you can get down to the main event---cooking!
+				We provide easy to use filters to search for your favorites, or browse by ingredient if you're undecided. 
+				Get started today by <a href="register.php">joining</a> or <a href="login.php">signing in</a>!
+				</p>
+			</div>
+			<div class="display">
+				<div id="screenshot1" class="screenshot">screenshot coming soon </div>
+				<div id="screenshot2" class="screenshot">screenshot coming soon </div>
+				<div id="screenshot3" class="screenshot">screenshot coming soon </div>
+			</div>
+			<div id="signUp" class="plainText">
+				Interested? <a href="register.php">Join</a> today!
 			</div>
 		</div>
 <?php include_once("footer.php"); ?>
