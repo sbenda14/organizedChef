2e72b01be2731236659fda2addbfdbdee978aace
sbenda14 <susanbenda@u.boisestate.edu>
2020-02-18 20:44:43 -0700

    recipes sidebar

--- recipes.php
+++ recipes.php
@@ -1,5 +1,25 @@
 <?php include_once("header.php"); ?>
 		<div class="content">
+			<div class="sideBar">
+				<!--These will have forms underneath them: check boxes, dropdowns, etc.
+					these are placeholders-->
+				<p> Add a Recipe </p>
+				<hr></hr>
+				<div class="filters">
+					Search by Title:
+				</div>
+				<div class="filters">
+					Filter by Category:
+				</div>
+				<div class="filters">
+					Filter by Ingredient:
+				</div>
+				<div class="filters">
+					Filter by Cook Time:
+				</div>
+			</div>
+			<div class="recipes">
+				query results will go here
+			</div>
 		</div>
-		
 <?php include_once("footer.php"); ?>
