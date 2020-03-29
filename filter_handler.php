a45d015b4a42037e08dafac3d9114d8c3d3b7693
sbenda14 <susanbenda@u.boisestate.edu>
2020-03-29 11:51:53 -0600

    filter options

---
+++ filter_handler.php
@@ -0,0 +1,26 @@
+<?php
+  session_start();
+  require_once 'Dao.php';
+  $dao = new Dao();
+
+  $_SESSION['filter'] = TRUE;
+  if(empty($_POST['filterTitle'])){
+	$_SESSION['filterTitle']= "%";
+  }else{
+	$_SESSION['filterTitle']= "%" . $_POST['filterTitle'] . "%";
+  }
+  if(empty($_POST['filterCategory'])){
+	$_SESSION['filterCategory']= "%";
+  }else{
+	$_SESSION['filterCategory']= $_POST['filterCategory'];
+  }
+  if(empty($_POST['filterIngredient'])){
+	$_SESSION['filterIngredient']= "%";
+  }else{
+	$_SESSION['filterIngredient']= "%". $_POST['filterIngredient'] . "%";
+  }
+
+  header("Location: https://theorganizedchef.herokuapp.com/recipes.php");//send to recipe page
+  exit;
+  
+  
