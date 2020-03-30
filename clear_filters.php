b34f7402597367e2a453b9021bba8ef993f3ca21
sbenda14 <susanbenda@u.boisestate.edu>
2020-03-30 11:23:10 -0600

    renamed file

--- clear_filters.php
+++
@@ -1,8 +0,0 @@
-<?php
-  session_start();
-  require_once 'Dao.php';
-  $dao = new Dao();
-
-  $_SESSION['filter'] = FALSE;
-  header("Location: https://theorganizedchef.herokuapp.com/recipes.php");//send to recipe page
-  exit;
