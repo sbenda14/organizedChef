943386f7fdbe51424cef7f2d1ecb172d1183a94a
sbenda14 <susanbenda@u.boisestate.edu>
2020-12-18 14:37:15 -0700

    composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3d9936b0890e226e1996c3f80cf5a32bf90bc026',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3d9936b0890e226e1996c3f80cf5a32bf90bc026',
+    ),
+  ),
+);
