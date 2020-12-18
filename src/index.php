f6a628aab3d5dd094c91e5ae805ed7f935c782f9
sbenda14 <susanbenda@u.boisestate.edu>
2020-12-18 15:53:54 -0700

    edit paths

--- src/index.php
+++ src/index.php
@@ -1,12 +1,12 @@
 <?php 
   session_start();
-  include_once("src/components/banner.php");
+  include_once("components/banner.php");
   if(!isset($_SESSION['auth']) || !$_SESSION['auth']){
-    include_once("src/components/joinUs.php");
+    include_once("components/joinUs.php");
   }else	{
-	include_once("src/components/leaveUs.php");
+	include_once("components/leaveUs.php");
    }
-  include_once("src/components/navBar.php");
+  include_once("components/navBar.php");
  ?>
 	<div class="content plainText" id="home">
 		<h1 class="welcome">Welcome to The Organized Chef!</h1>
@@ -19,16 +19,16 @@
 			This webpage is dedicated to reducing the time you
 			spend searching for your favorite recipes. With everything in one place,you can get down to the main event---cooking!
 			We provide easy to use filters to search for your favorites, or browse by ingredient if you're undecided. 
-			Get started today by <a href="src/login/register.php">joining</a> or <a href="src/login/login.php">signing in</a>!
+			Get started today by <a href="login/register.php">joining</a> or <a href="login/login.php">signing in</a>!
 		</p>
 		<p>With the Organized Chef, you can:</p>
 		<div class="display">
-			<div class="screenshot"><p>Add a recipe or filter by title, ingredient or category:</p> <img class="demo" src="images/sidebar.png"></div>
-			<div  class="screenshot"><p>View all recipes, or a filtered selection:</p><img class="demo" src="images/recipeList.png"></div>
-			<div  class="screenshot"><p>View recipe on page, or follow link to original page:</p><img class="demo" src="images/recipeView.png"></div>
+			<div class="screenshot"><p>Add a recipe or filter by title, ingredient or category:</p> <img class="demo" src="../images/sidebar.png"></div>
+			<div  class="screenshot"><p>View all recipes, or a filtered selection:</p><img class="demo" src="../images/recipeList.png"></div>
+			<div  class="screenshot"><p>View recipe on page, or follow link to original page:</p><img class="demo" src="../images/recipeView.png"></div>
 		</div>
 		<div id="signUp" class="plainText">
-				Interested? <a href="src/login/register.php">Join</a> today!
+				Interested? <a href="login/register.php">Join</a> today!
 		</div>
 	</div>
-<?php include_once("src/components/footer.php"); ?>
+<?php include_once("components/footer.php"); ?>
