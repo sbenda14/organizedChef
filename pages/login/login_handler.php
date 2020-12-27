a2f8c9d96b5715cbb6ad77cc2e307b0fe3dcabfe
sbenda14 <susanbenda@u.boisestate.edu>
2020-12-27 12:54:59 -0700

    log stmts

--- pages/login/login_handler.php
+++ pages/login/login_handler.php
@@ -1,15 +1,24 @@
 <?php
   session_start(); 
+  
+  use Monolog\Logger;
+  use Monolog\Handler\StreamHandler;
+  $log = new Logger('login');
+  //use php://stderr to connect log to Heroku logs
+  $log->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
+  
   require_once '../../server/Dao.php';
   $dao = new Dao();
   
   $errors = array();
  //validate email is an email and password is not blank
  if(!preg_match("/^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,5}$/",$_POST['email'])){
-	$errors[] = "Please enter a valid email address, e.g. name@gmail.com"; 
+	$errors[] = "Please enter a valid email address, e.g. name@gmail.com";
+	$log->info('invalid email format');
  }
  if($_POST['password'] == ""){
 	$errors[] = "Password cannot be blank"; 
+	$log->info('no password entered');
  }
  
  if(0 < count($errors)){
@@ -28,6 +37,7 @@
   if($matchUser == 0) {
     $_SESSION['logForm'] = $_POST;
 	$errors[] = "Error, email or password do not match";
+	$log->warn('Email or password did not match', ['email' => $_POST['email']]);
     $_SESSION['errors'] = $errors;
 	$_SESSION['auth']=false;
     header("Location: https://theorganizedchef.herokuapp.com/pages/login/login.php"); 
@@ -37,6 +47,7 @@
   //If user exists and password valid, then go to recipe page
   unset($_SESSION['logForm']);
   $_SESSION['user']= $matchUser; //I know this isn't secure. will need to adjust
+  $log->info('user is logged in', ['email' => $_POST['email']]);
   $_SESSION['auth']=true;
   header("Location: https://theorganizedchef.herokuapp.com/pages/recipes/recipes.php");
   exit;
